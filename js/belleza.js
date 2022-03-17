da859130654a2a6a8f980606b6a403db57a3cc0c
Mariana Camacho <marianacamacho700@gmail.com>
2022-03-16 23:59:17 -0500

    Avance Proyecto Final

---
+++ js/belleza.js
@@ -0,0 +1,58 @@
+let productsBelleza = [
+    { 
+        id: 1,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 2,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 3,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 4,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 5,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 6,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 7,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+    { 
+        id: 8,
+        img: "../images/products/olla-cera.webp",
+        title: "olla-cera",
+        precio:0 ,
+        description: "The Archer Queen is essentially a more powerful version of the Archer. She is the second Hero, being unlocked at Town Hall 9. In addition to possessing much higher damage and hitpoints, the Archer Queen also shoots 1.5 tiles farther than a normal Archer (5 tiles instead of 3.5 tiles)"
+    },
+]
